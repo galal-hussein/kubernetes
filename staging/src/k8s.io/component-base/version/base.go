4461ecf62cd631390a8e6a1abcbdc4cb7ace6550
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2024-08-13 04:01:48 +0300

    v1.30.3-k3s2

--- staging/src/k8s.io/component-base/version/base.go
+++ staging/src/k8s.io/component-base/version/base.go
@@ -4,7 +4,7 @@ var (
 	gitMajor = "1"
 	gitMinor = "30"
 	gitVersion   = "v1.30.3-k3s2"
-	gitCommit    = "a3b9bcebcdef3764e8f00240aea96c0c358e6986"
+	gitCommit    = "1f6f685daf3c021c15ed8192277fdd5cbe53e172"
 	gitTreeState = "clean"
-	buildDate = "2024-08-09T23:06:54Z"
+	buildDate = "2024-08-13T01:00:10Z"
 )
